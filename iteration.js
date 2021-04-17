68ecb67c1b8cfedd9b87b6c232d75ac0e49fb166
fschweikhardt <schweikhardt.web@gmail.com>
2021-04-17 13:43:21 -0400

    Update iteration.js

--- iteration.js
+++ iteration.js
@@ -1,3 +1,12 @@
 'DSA Iteration'
 
 '1. Counting Sheep'
+
+function countingSheep(num) {
+    for (let i = num; i > 0; i--) {
+        console.log(`${i} sheep jumped over the fence...`)
+    }
+    return console.log('All the sheep jumped over the fence')
+}
+
+countingSheep(3)
