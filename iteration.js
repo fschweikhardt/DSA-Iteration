82bdfb866c6a36652f6ef10d7bd78a70584d9a1a
fschweikhardt <schweikhardt.web@gmail.com>
2021-04-17 17:00:48 -0400

    Update iteration.js

--- iteration.js
+++ iteration.js
@@ -11,6 +11,7 @@ function countingSheep(num) {
 
 //countingSheep(3)
 
+
 '2. Power Calculator'
 
 function powerCalculator(base, exponent) {
@@ -24,6 +25,7 @@ function powerCalculator(base, exponent) {
     return num
 }
 //console.log(powerCalculator(10,2))
+
 
 '3. Reverse String'
 
@@ -54,6 +56,7 @@ function reverseArray(string) {
 
 //console.log(reverseArray(string3))
 
+
 '4. nth Triangular Number'
 
 //n = number of sides
@@ -70,5 +73,92 @@ function triangleNumber(n) {
 
 //console.log(triangleNumber(6))
 
+
 '5. String Splitter'
 
+let inputDate = '02/20/2020/'
+//Output: ["02", "20", "2020"]
+
+//with .split()
+//create array
+//split at '/'
+
+function stringWithSplit(input) {
+    return input.split('/')
+}
+
+//console.log(stringWithSplit(inputDate))
+
+//index through the string
+//if string[i] === '/'
+
+function stringSplitter(input) {
+    let arr = []
+    let newArr = []
+    let string = ''
+    for (let i = 0; i < input.length; i++) {
+        arr.push(input[i])
+    }
+    for (let i = 0; i < arr.length; i++) {
+        console.log(string)
+        console.log(newArr)
+        if (arr[i] === '/') {
+            newArr.push(string)
+            string = ''
+        }
+        string += arr[i]
+    }
+    return newArr
+}
+
+//console.log(stringSplitter(inputDate))
+
+
+'6. Fibonacci'
+
+//let x
+//let y
+//let total
+//takes an nth
+//returns a total
+
+function Fibonacci(nth) {
+    let x = 1
+    let y = 0
+    let z
+    if (nth <= 0) { 
+        return console.log('must be positve integer')
+    }
+    if (nth === 1 || nth === 2) {
+        return 1
+    }
+    while (nth > 0) {
+        z = x
+        x = x + y
+        y = z
+        nth--
+    }
+    return y
+}
+
+//console.log(Fibonacci(7))
+
+
+'7. Factorial'
+
+function Factorial(num) {
+    if (num <= 0) {
+        return 'must be positive integer'
+    }
+    let total = 1
+    while (num > 0) {
+        total = total * num
+        num--
+    }
+    return total
+}
+
+console.log(Factorial(5))
+
+
+'8. Find a way out of the maze'
